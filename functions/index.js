6f67db0045e2a82b349dc251c6728c608d622a71
MeshachArinze <meshacharinze@gmail.com>
2022-10-12 03:59:03 +0100

    refactor to github

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
 const functions = require("firebase-functions");
 
 const {Telegraf} = require("telegraf");
-
+const apixu = require("apixu");
+const apixuClient = new apixu.Apixu({
+  apikey: "b4676696af281aab7fbc1e0f2e97e1d0",
+});
 const bot = new Telegraf("5440756045:AAEyrqh0VJxFu0tHV42eS3WyiDZkVa1yhL8");
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
@@ -13,6 +16,9 @@ bot.launch();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  apixuClient.current("London").then((current) => {
+    return response.send(current);
+  }).catch((err) => {
+    return response.send(err);
+  });
 });
